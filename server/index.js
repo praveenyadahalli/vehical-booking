338eac946c199c3f9f86409c413b3e4dcf650db2
Praveen Yadahalli <58703548+praveenyadahalli@users.noreply.github.com>
2025-03-05 20:34:30 +0530

    added gitignore,db-connect,index file and other file.

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db-connect");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(5000, () => {
+  console.log("server has started on port 5000");
+});
